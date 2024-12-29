c717e4446545e7d27f86a5d83915ae8a7c40f3cc
OxJaren <lucky.pinem01@gmail.com>
2024-12-29 10:59:05 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // pastikan globals.browser tidak memiliki kesalahan konfigurasi
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
